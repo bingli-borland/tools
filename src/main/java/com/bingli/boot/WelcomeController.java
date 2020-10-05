a2a27abd85d678d96155baf9bc870488a677ca66
bingli-borland <1042996632@qq.com>
2020-10-05 15:54:08 +0800

    + parse gsq

--- src/main/java/com/bingli/boot/WelcomeController.java
+++ src/main/java/com/bingli/boot/WelcomeController.java
@@ -140,6 +140,33 @@ public class WelcomeController {
         return "index";
     }
 
+    @GetMapping("/ParseGsq")
+    public String ParseGsq(Map<String, Object> model) throws IOException {
+        List<ArticleBean> rs = new ArrayList<ArticleBean>();
+        String datafile = ParseOption.class.getResource("/gsq.html").getPath();
+        File in = new File(datafile);
+        Document doc = Jsoup.parse(in, "utf-8");
+        Elements es = doc.select("div").select(".js_card");
+        for (int i = 0; i < es.size(); i++) {
+            String datetime = es.get(i).select(".weui_msg_card_hd").first().text();
+            Element e = es.get(i).select(".weui_media_title").first();
+            if (e == null) {
+                
+                continue;
+            }
+            String url = e.attr("hrefs");
+            String title = e.text();
+            ArticleBean b = new ArticleBean();
+            b.setContent_url(url);
+            b.setDatetime(datetime);
+            b.setTitle(title);
+            rs.add(b);
+            //System.out.println(b);
+        }
+        //Collections.sort(rs);
+        model.put("beans", rs);
+        return "index";
+    }
     @RequestMapping("/foo")
     public String foo(Map<String, Object> model) {
         throw new RuntimeException("Foo");
